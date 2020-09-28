6d926d375eddfa3eb5d541fea40948ede9837b7f
Oskari Virtaoja <oskari@swarmia.com>
2020-09-28 18:54:35 +0300

    WIP

---
+++ backend/src/knex.ts
@@ -0,0 +1,23 @@
+import * as Knex from 'knex'
+
+import environment from './environment'
+
+export const config = {
+  client: 'pg',
+  connection: environment.databaseUrl,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: 'migrations'
+  },
+  timezone: 'UTC'
+}
+
+function connect(): Knex {
+  return Knex(config)
+}
+
+export default connect()
